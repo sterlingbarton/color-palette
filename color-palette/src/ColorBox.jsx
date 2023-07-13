376deacc2178ba69a6ba6e0b4bf77e24f590100c
Sterling <sterlingbarton81@gmail.com>
2023-07-13 15:14:33 -0500

    further color box styling

--- color-palette/src/ColorBox.jsx
+++ color-palette/src/ColorBox.jsx
@@ -2,9 +2,14 @@ import './ColorBox.css'
 
 export default function ColorBox({background, name}) {
   return (
-    <div style={{background: background }} className="color-box">
-        <span>{name}</span>
-        <span>MORE</span>
+    <div style={{background}} className="color-box">
+        <div className='copy-container'>
+            <div className='box-content'>
+                <span>{name}</span>
+            </div>
+            <button className='copy-button'>Copy</button>
+        </div>
+        <span className='see-more'>MORE</span>
     </div>
   )
 }
