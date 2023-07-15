77fd950c4c1fc1ab991d45c30df58c2238afb2a3
Sterling <sterlingbarton81@gmail.com>
2023-07-15 11:01:25 -0600

    added copy-to-clipboard with animation

--- color-palette/src/ColorBox.jsx
+++ color-palette/src/ColorBox.jsx
@@ -1,16 +1,31 @@
+import {useState} from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css'
 
 export default function ColorBox({background, name}) {
+    const [copied, setCopied] = useState(false)
+
+    function changeCopyState(){
+        setCopied(true)
+        setTimeout(() => setCopied(false), 1500)
+    }
+
   return (
-    <div style={{background}} className="color-box">
-        <div className='copy-container'>
-            <div className='box-content'>
-                <span>{name}</span>
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+        <div style={{background}} className="color-box">
+            <div style={{background}} className={`copy-overlay ${copied && 'show'}`}/>
+            <div className={`copy-msg ${copied && 'show'}`}>
+                <h1>copied!</h1>
+                <p>{background}</p>
             </div>
-            <button className='copy-button'>Copy</button>
+            <div className='copy-container'>
+                <div className='box-content'>
+                    <span>{name}</span>
+                </div>
+                <button className='copy-button'>Copy</button>
+            </div>
+            <span className='see-more'>MORE</span>
         </div>
-        <span className='see-more'>MORE</span>
-    </div>
+    </CopyToClipboard>
   )
 }
-
