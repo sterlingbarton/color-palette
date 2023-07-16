04c7e2fd0451aea7de27c49457cd2d6b0acc4306
Sterling <sterlingbarton81@gmail.com>
2023-07-16 16:10:28 -0600

    initial jss styles on palette list

--- color-palette/src/PaletteList.jsx
+++ color-palette/src/PaletteList.jsx
@@ -1,16 +1,55 @@
 import React from 'react'
-// import {Link} from 'react-router-dom'
 import MiniPalette from './MiniPalette'
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    root: {
+        backgroundColor: 'blue',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap'
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 30%)',
+        gridGap: '5%'
+    },
+
+})
 
 export default function PaletteList({palettes}) {
+    const classes = useStyles()
 
     const paletteList = palettes.map(palette => {
         return <MiniPalette key={palette.paletteName} palette={palette}/>
     })
 
   return (
-    <div>
-        {paletteList}
+    <div className={classes.root}>
+        <div className={classes.container}>
+            <nav className={classes.nav}>
+                <h1>React Colors</h1>
+            </nav>
+            <div className={classes.palettes}>
+                {paletteList}
+            </div>
+        </div>
     </div>
   )
 }
