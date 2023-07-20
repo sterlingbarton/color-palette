9460df67361bf1d56636901e854e971af4a50d29
Sterling <sterlingbarton81@gmail.com>
2023-07-20 11:53:47 -0600

    styled draggable color box

--- color-palette/src/DraggableColorBox.jsx
+++ color-palette/src/DraggableColorBox.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
 import './DraggableColorBox.css'
 
 export default function DraggableColorBox({color, name}) {
   return (
-    <div className='root' style={{backgroundColor:color}}>{name}</div>
+    <div className='root' style={{backgroundColor:color}}>
+        <div className='box-content'>
+            <span>{name}</span>
+            <DeleteIcon className='delete-icon'/>
+        </div>
+    </div>
   )
 }
 
